faa1ef65ed042b96da7c0975588655f06e9a4f63
gregor <sirgregordevelope@gmail.com>
2022-02-16 15:05:47 +0300

    Texture update

--- core/src/gregor/kanakov/flappybirdclone/FlappyBird.java
+++ core/src/gregor/kanakov/flappybirdclone/FlappyBird.java
@@ -1,31 +1,49 @@
 package gregor.kanakov.flappybirdclone;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+
+	Texture[] bird;
+
+	int birdLocation = 2;
+	int birdStateFlag = 0;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
+		bird = new Texture[2];
+		bird[0] = new Texture("bird_wings_up.png");
+		bird[1] = new Texture("bird_wings_down.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		if(birdStateFlag == 0){
+			birdStateFlag = 1;
+			birdLocation = 2;
+		}else {
+			birdStateFlag = 0;
+			birdLocation = 3;
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());// установка background
+		batch.draw(bird[birdStateFlag], Gdx.graphics.getWidth() /2 -
+						bird[birdStateFlag].getWidth() /2,
+				Gdx.graphics.getHeight()/2 -
+						bird[birdStateFlag].getHeight() /2); //отцентровка изображения птицы
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
